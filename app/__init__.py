9d7162da39a95b3c83dbd7030f70e34321056781
guylainkerakabo <guylain.kerakabo2000@gmail.com>
2020-02-23 11:32:49 +0200

    initialisation des template

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ import os
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+#from flask_login import LoginManager
 
 from config import app_config
 
 db = SQLAlchemy()
-login_manager= LoginManager()
+#login_manager= LoginManager()
 
 def create_app(config_name):
     # create and configure the app
@@ -18,7 +18,7 @@ def create_app(config_name):
     # enregistrement des extension 
     db.init_app(app)
     migrate = Migrate(app, db)
-    login_manager.init_app(app)
+    #login_manager.init_app(app)
     # enregistrement des blueprint
     from . import authentification
     app.register_blueprint(authentification.bp)
